149d31862247e4af4929cc60e5933fc724950887
xxx407410849 <407410849@qq.com>
2018-08-15 16:46:29 +0800

    react_cli_version1.0.1

---
+++ package.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './public/less/index.less';
+
+ReactDOM.render(
+    <div>
+        <h1>你真是一个气死人的小可爱啊</h1>,
+    </div>,
+    document.getElementById('reactRoot')
+)
